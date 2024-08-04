a4f157b47904f57acf677bf19818215f973ce60d
Santiago Cianciulli <santiagoc81@outlook.com>
2024-08-04 13:42:27 -0700

    Instalacion firebase

---
+++ chanchustudio/src/components/script-firebase.jsx
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyB2EzzeIo50Wk1DJGMMByDj8O307ujrZfg",
+  authDomain: "chanchustudio-4d93f.firebaseapp.com",
+  projectId: "chanchustudio-4d93f",
+  storageBucket: "chanchustudio-4d93f.appspot.com",
+  messagingSenderId: "1038822906134",
+  appId: "1:1038822906134:web:71f76e53223b1c3801d837"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
